ffcef8f3b973c90f4daa6a26d6d3b9f6611f2410
Hsn9403 <julienhassan11@gmail.com>
2025-07-29 00:12:50 +0200

    Update verif_joueurs_laliga_SAFE_MATCH.py

--- verif_joueurs_laliga_SAFE_MATCH.py
+++ verif_joueurs_laliga_SAFE_MATCH.py
@@ -167,7 +167,8 @@ def home():
     return "✅ API Flask déployée avec succès ! Utilise /analyze pour envoyer un CSV.", 200
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=10000)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
 add home route for Render status check
 
